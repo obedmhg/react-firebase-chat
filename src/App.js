7911e61dd1c9ed0737c9b8445ae88095a51b0811
Obed Murillo <obedmhg@gmail.com>
2021-10-28 16:24:43 -0600

    adding config

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 })
 
 const auth = firebase.auth();
